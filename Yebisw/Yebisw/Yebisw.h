5392f0b1ce4e1010d2447fa182856d6e1618c1e1
gaopin1534 <gaoxin.xixxix@gmail.com>
2018-01-16 11:49:27 +0900

    init project

---
+++ Yebisw/Yebisw/Yebisw.h
@@ -0,0 +1,19 @@
+//
+//  Yebisw.h
+//  Yebisw
+//
+//  Created by 高松　幸平 on 2018/01/16.
+//  Copyright © 2018年 高松　幸平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Yebisw.
+FOUNDATION_EXPORT double YebiswVersionNumber;
+
+//! Project version string for Yebisw.
+FOUNDATION_EXPORT const unsigned char YebiswVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yebisw/PublicHeader.h>
+
+
