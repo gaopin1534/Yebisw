7ecfdf6a93d2ddb9a17fef74a496052e4cc5db93
高松幸平 <gaoxin.xixxix@gmail.com>
2018-02-24 20:59:04 +0900

    add Makefile thanks to DIKit

---
+++ Sources/YebiswGenSupport/Version.swift
@@ -0,0 +1,13 @@
+//
+//  Version.swift
+//  Yebisw
+//
+//  Created by 高松幸平 on 2018/02/24.
+//  Copyright © 2018年 高松　幸平. All rights reserved.
+//
+
+import Foundation
+
+public final struct Version {
+    public static let current = "0.1.0"
+}
