2aa631fd4d1b3b3269682db7e268911133333589
gaopin1534 <gaoxin.xixxix@gmail.com>
2018-03-06 17:10:02 +0900

    add example skelton

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 高松　幸平 on 2018/03/06.
+//  Copyright © 2018年 gaopin1534. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
