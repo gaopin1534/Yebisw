21df13418445f4c13fe6753b93eb61ba46d8e90d
高松幸平 <gaoxin.xixxix@gmail.com>
2018-04-12 13:54:19 +0900

    initial commit

---
+++ SecondTests/HogeTest.swift
@@ -0,0 +1,33 @@
+//
+//  HogeTest.swift
+//  YebiswTests
+//
+//  Created by 高松幸平 on 2018/03/13.
+//
+
+import XCTest
+
+class HogeTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertEqual("Hello, World!", "Hello, World!")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
--- Tests/YebiswTests/YebiswTests.swift
+++ Tests/YebiswTests/YebiswTests.swift
@@ -6,7 +6,7 @@ class YebiswTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Yebisw().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
 
