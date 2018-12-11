3811daa4abc533106e638a83039c640e318d0e59
gaopin1534 <gaoxin.xixxix@gmail.com>
2018-12-11 16:53:25 +0900

    rename cli

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
 //         Targets are the basic building blocks of a package. A target can define a module or a test suite.
 //         Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "YebiswGen",
+            name: "yebiswgen",
             dependencies: ["Yebisw", "SourceKittenFramework", "Commander"],
-            path: "Sources/YebiswGen"),
+            path: "Sources/yebiswgen"),
         .target(name: "Yebisw",
                 path: "Sources/Yebisw"),
         .testTarget(
